b6674f4d88b844490262a059c9597fd136087d4c
franc <francis.lewis07@gmail.com>
2021-11-04 20:12:18 -0400

    Finalisation de la classe convertir.

--- src/NouveauFichier.java
+++ src/NouveauFichier.java
@@ -8,13 +8,19 @@ public class NouveauFichier {
         nomFichier = nomFichier.replaceAll(".txt", ".html");
         FileWriter nouveauFichier = new FileWriter(nomFichier);
         nouveauFichier.write("<!DOCTYPE html>\n" +
-                "<html lang =" + "jv" + ">\n" +
+                "<html>\n" +
                 " <head>\n" +
                 " <title>TP 2</title>\n" +
+                "<style>\n" +
+                "p {\n writing-mode: vertical-rl;\n text-orientation: upright;\n }" +
+  "</style>\n" +
                 " </head>\n" +
-                " <body>\n‘" + NouveauFichier.blocLignes(d) + "’\n" +
+                " <body>\n" +
                     " <hr>\n" +
                     " <table>\n" +
+                    "<tr>\n" +
+                NouveauFichier.blocLignes(d) + "\n" +
+                    "</tr>\n" +
                     " </table>\n" +
                     "<hr>\n" +
                     "</body> \n" +
@@ -27,7 +33,7 @@ public class NouveauFichier {
         String newString = "";
 
         for(int i = 0; i < ligne.size(); i++){
-            newString = newString + "&#" + ligne.get(i) + " ";
+            newString = newString + "&#" + ligne.get(i) + "; ";
         }
         return newString;
     }
@@ -36,7 +42,7 @@ public class NouveauFichier {
         String bloc = "";
 
         for(int i = 0; i < d.size(); i++){
-            bloc = bloc + NouveauFichier.ligneString(d.get(i)) + "\n";
+            bloc = bloc + "<td><p>" + NouveauFichier.ligneString(d.get(i)) + "</p></td>\n";
         }
         return bloc;
     }
